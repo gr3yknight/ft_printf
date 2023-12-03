716b841d3b2a0c5a321c97f26784235e23891065
gr3yknight <hamza1talhaoui@gmail.com>
2023-12-03 21:43:00 +0100

    The main function

--- ft_printf.h
+++ ft_printf.h
@@ -4,5 +4,13 @@
 
 # include "libft.h"
 
+int     ft_printf(char const *str, ...);
+
+int		ft_putnbr(long nb);
+int     ft_putchar(char c);
+int     ft_putstr(char *str);
+int		ft_putptr(unsigned long l);
+int		ft_spf(char spf, va_list args);
+
 
 #endif 
--- ft_putnbr.c
+++ ft_putnbr.c
@@ -2,21 +2,27 @@
 
 int ft_putnbr(long nb)
 {
-    if (nb < 0)
+	static int	c;
+
+	c = 0;
+	if (nb < 0)
 	{
 		nb *= -1;
 		ft_putchar('-');
 	}
 	if (nb >= 10)
 		ft_putnbr(nb / 10);
-	return (ft_putchar(nb % 10 + '0'));
+	return (c += ft_putchar(nb % 10 + '0'));
 }
 
 int main()
 {
-	int k = ft_putnbr(9541);
-	printf("\n%d\n", k);
-	int j = putnbr(9541);
-	printf("\n%d\n", j);
-
+	int h = 9541;
+	int k = ft_putnbr(h);
+	printf("\n");
+	printf("%d", k);
+	printf("\n");
+	// int j = putnbr(h);
+	// printf("\n");
+	// printf("%d", j);
 }
