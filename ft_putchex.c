ad4fae8f16df582b4b0a0a020d0f9bece9d944b5
gr3yknight <hamza1talhaoui@gmail.com>
2023-12-04 22:21:02 +0100

    Upper Hex

--- ft_putchex.c
+++ ft_putchex.c
@@ -4,5 +4,5 @@ void ft_putchex(unsigned long l, int *c)
 {
 	if (l >= 16)
 		ft_putchex(l / 16, c);
-	return (ft_putchar("0123456789ABCDEF"[l % 16]), c);
+	return (ft_putchar("0123456789ABCDEF"[l % 16], c));
 }
