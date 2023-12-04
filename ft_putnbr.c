8a860768d94c0e0d7c64050ba7dcc2bd3ac3fee9
gr3yknight <hamza1talhaoui@gmail.com>
2023-12-04 02:04:52 +0100

    Was Not Asked to handle long

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-int ft_putnbr(long nb)
+int ft_putnbr(long int	nb)
 {
 	static int	c;
 
@@ -8,9 +8,9 @@ int ft_putnbr(long nb)
 	if (nb < 0)
 	{
 		nb *= -1;
-		ft_putchar('-');
+		c += ft_putchar('-');
 	}
 	if (nb >= 10)
-		ft_putnbr(nb / 10);
+		c += ft_putnbr(nb / 10);
 	return (c += ft_putchar(nb % 10 + '0'));
 }
--- ft_unsigned.c
+++ ft_unsigned.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-int ft_unsigned(unsigned long nb)
+int ft_unsigned(unsigned int nb)
 {
 	static int  c;
 
@@ -9,14 +9,12 @@ int ft_unsigned(unsigned long nb)
 	return (c);
 }
 
-int main()
-{
-	unsigned int nb = -19;
-	int j = ft_unsigned(nb);
-	printf("\n");
-	int k = printf("%u", nb);
-	printf("\n");
-	printf("%d", j);
-	printf("\n");
-	printf("%d", k);
-}
+// int main()
+// {
+// 	unsigned int nb = INT_MAX + 1;
+// 	ft_unsigned(nb);
+// 	printf("\n");
+// 	// printf("my return value :[%d]\n", j);
+// 	// printf("%u\n", nb);
+// 	// printf("original return value :[%d]\n", k);
+// }
